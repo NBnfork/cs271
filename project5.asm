4198df674315fb466d8ae1fc2f086a44651aa1ca
NBnfork <nbuchen@gmail.com>
2018-03-04 08:38:14 -0500

    procs commeted and outlined

--- project5.asm
+++ project5.asm
@@ -17,17 +17,18 @@ HI = 999
 
 .data
 mess1       BYTE    "Random Integer Sort by Noah Buchen", 13, 10,0
-mess2		BYTE	"This program generates random numbers in the",
+mess2		BYTE	"This program generates random numbers in the ",
 					"range [100... 999],", 13, 10,
 					"displays the original list, sorts the list, ",
 					"calculates the ", 13, 10,
 					"median value, and displays the sorted list.", 13, 10, 0
 prompt1		BYTE	"How many numbers should be generated? [10-200]: ", 13, 10, 0
 unsortMess	BYTE	"The unsorted random numbers:", 13, 10, 0
-medianMess	BYTE	"The median is ", 13, 10, 0
 sortMess	BYTE	"The sorted list:", 13, 10, 0
+medianMess	BYTE	"The median is ", 13, 10, 0
+
 userInput	DWORD	? ;number of ints to display
-theArray	DWORD	? ;array for random numbers 
+theArray	DWORD	UPPERLIMIT	DUP(?) ;array for random numbers 
 
 .code
 main PROC
@@ -58,26 +59,62 @@ intro PROC
 	ret
 intro	ENDP
 
-getData	PROC
+;Procedure to gather data from user.
+;receives: OFFSET userInput
+;returns: userInput
+;preconditions:
+;registers changed:
 
+getData	PROC
+ 
 	ret
 getData	ENDP
 
+;Procedure to fill array with random numbers
+;receives: OFFSET theArray, userInput
+;returns: 
+;preconditions: userInput <= 10
+;registers changed:
 fillArray	PROC
 
 	ret
 fillArray	ENDP
 
+;Procedure to display each element of the array.
+;receives: OFFSET theArray, userInput, OFFSET unsortedMess, OFFSET sortedMess
+;returns: 
+;preconditions: array size = userInput
+;registers changed:
 displayList	PROC
 
 	ret
 displayList	ENDP
 
+;Procedure to sort the list into decesending order
+;receives: OFFSET theArray, userInput
+;returns: 
+;preconditions: array size = userInput
+;registers changed:
 sortList	PROC
 
 	ret
 sortList	ENDP
 
+;Procedure to exchange array elements for selection sort.
+;receives: OFFSET theArray[i], OFFSET theArray[j] (i and j are elements to be compared)
+;returns: 
+;preconditions: i and j must be > userInput - 1
+;registers changed:
+exchange	PROC
+
+	ret
+exchange	ENDP
+
+;Procedure calculate the  median value.
+;receives: userInput, OFFSET theArray 
+;returns: userInput
+;preconditions: theArray is sorted
+;registers changed:
 displayMedian	PROC
 
 	ret
